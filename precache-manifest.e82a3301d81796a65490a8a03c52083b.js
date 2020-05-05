e058a3e8ce5b6caf8fa3bd6718490654278181e1
Rap Ramos <rap.ramos@htw.com.au>
2020-05-05 21:10:16 +0800

    Updates

---
+++ precache-manifest.e82a3301d81796a65490a8a03c52083b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "badca6d5fcd980a7ae88",
+    "url": "/portfolio/static/js/main.badca6d5.chunk.js"
+  },
+  {
+    "revision": "e637ced2e40eb0d3d952",
+    "url": "/portfolio/static/js/1.e637ced2.chunk.js"
+  },
+  {
+    "revision": "badca6d5fcd980a7ae88",
+    "url": "/portfolio/static/css/main.9d9c5661.chunk.css"
+  },
+  {
+    "revision": "3c025bd9b8191810766dfdef35e9c250",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35415f8d36a2674d4c551fa0f02d93e8.js"
+  "/portfolio/precache-manifest.e82a3301d81796a65490a8a03c52083b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
